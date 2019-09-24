4d67147a4cee1c4ebf99a1e4aba714ea7649e96c
Mondo <568197478@qq.com>
2019-09-24 10:03:15 +0800

    fix 创建

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,30 @@
+module.exports = {
+  base: '/command/',
+  title: 'fe-dev-command',
+  description: '前端开发实践中所知的SSH命令',
+  head: [['link', { rel: 'icon', href: '/favicon.ico' }]],
+  themeConfig: {
+    smoothScroll: true,
+    nav: [
+      {
+        text: '指南',
+        link: '/guide/Shell'
+      },
+      {
+        text: 'Github',
+        link: 'https://github.com/one-pupil/fe-dev-command'
+      }
+    ],
+    sidebar: {
+      '/guide/': [
+        {
+          collapsable: false,
+          children: [
+            'Shell',
+            'Git'
+          ]
+        }
+      ]
+    }
+  }
+};
---
+++ docs/README.md
@@ -0,0 +1,13 @@
+---
+home: true
+actionText: 开始阅读 →
+actionLink: /guide/Shell/
+features:
+- title: 快捷
+  details: 可以快速查找到自己所需要的指令
+- title: 记录
+  details: 记录自己工作中用到的命令
+- title: 更新
+  details: 持续对命令行进行更新
+# footer: MIT Licensed | Copyright © 2019-present YKFE 版权所有
+---
